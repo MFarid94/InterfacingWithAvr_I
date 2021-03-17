d69b648a6044862c75394be73ac99e00f9055472
Muhammad Farid <mfhosni94@gmail.com>
2021-03-17 13:15:56 +0200

    Exercise 2

--- Exercise2/Exercise2.c
+++ Exercise2/Exercise2.c
@@ -8,7 +8,6 @@
 #include<avr/io.h>
 #include<util/delay.h>
 
-
 int main(void){
 	DDRD |= (1<<6);		/* Configure PIN 6 at PORTD as OUTPUT PIN */
 	PORTD |= (1<<6);  /* Initialize PIN 6  at PORTD to 1, LED OFF*/
@@ -21,5 +20,10 @@ int main(void){
 		PORTD |= (1<<6);
 
 		_delay_ms(1000);
+
+		/**** Another Solution ****
+		 * 		PORTD ^= (1<<6);
+		 * 		_delay_ms(1000);
+		 **************************/
 	}
 }
